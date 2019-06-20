2f30aa09cf4ccb7aec55d1caf9b2e7907f356161
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-06-20 17:51:41 -0500

    Creating a react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
+  const rows = () => [
+    <tr>
+      <td>rojasleon</td>
+      <td>rojasleon.dev@gmail.com</td>
+      <td>rojasleon.me</td>
+    </tr>,
+    <tr>
+      <td>rojasleon</td>
+      <td>rojasleon.dev@gmail.com</td>
+      <td>rojasleon.me</td>
+    </tr>,
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='margin'>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>{rows()}</tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
