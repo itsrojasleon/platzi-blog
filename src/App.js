f243448e67fe97f95e0e1cfd9f073e65b095526d
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-06-21 15:27:05 -0500

    Steteful vs Stateless

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  const rows = () => [
-    <tr>
-      <td>rojasleon</td>
-      <td>rojasleon.dev@gmail.com</td>
-      <td>rojasleon.me</td>
-    </tr>,
-    <tr>
-      <td>rojasleon</td>
-      <td>rojasleon.dev@gmail.com</td>
-      <td>rojasleon.me</td>
-    </tr>,
-  ];
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        {
+          name: 'Rojas León',
+          email: 'rojasleon.dev@gmail.com',
+          link: 'rojasleon.com',
+        },
+        {
+          name: 'Juan Luis Rojas',
+          email: 'rojasleon.dev@gmail.com',
+          link: '',
+        },
+        {
+          name: 'Platzi',
+          email: 'info@platzi.com',
+          link: 'platzi.com',
+        },
+      ],
+    };
+  }
 
-  return (
-    <div className='margin'>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{rows()}</tbody>
-      </table>
-    </div>
-  );
-};
+  ponerFilas = () =>
+    this.state.users.map(user => (
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.link}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div className='margen'>
+        <table className='tabla'>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>email</th>
+              <th>link</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default App;
