48891815a7dcc67ac8a655aa68e26228494de837
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-06-21 17:30:21 -0500

    Connect to a component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Users extends Component {
   state = { users: [], loading: false, error: false };
 
   async componentDidMount() {
-    this.setState({ loading: true });
-    try {
-      const { data } = await axios.get(
-        'http://jsonplaceholder.typicode.com/users',
-      );
-      this.setState({ users: data, loading: false });
-    } catch (error) {
-      this.setState({ error, loading: false });
-    }
+    // this.setState({ loading: true });
+    // try {
+    //   const { data } = await axios.get(
+    //     'http://jsonplaceholder.typicode.com/users',
+    //   );
+    //   this.setState({ users: data, loading: false });
+    // } catch (error) {
+    //   this.setState({ error, loading: false });
+    // }
   }
 
-  rows = () =>
-    this.state.users.map(user => (
-      <tr key={user.id}>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.website}</td>
-      </tr>
-    ));
+  // rows = () =>
+  //   this.state.users.map(user => (
+  //     <tr key={user.id}>
+  //       <td>{user.name}</td>
+  //       <td>{user.email}</td>
+  //       <td>{user.website}</td>
+  //     </tr>
+  //   ));
 
   render() {
+    console.log(this.props);
     return (
       <div className='margin'>
-        {this.state.loading && <span>Loading..</span>}
+        {/* {this.state.loading && <span>Loading..</span>} */}
         <table className='table'>
           <thead>
             <tr>
@@ -37,11 +38,13 @@ class Users extends Component {
               <th>Website</th>
             </tr>
           </thead>
-          {!this.state.loading && <tbody>{this.rows()}</tbody>}
+          {/* {!this.state.loading && <tbody>{this.rows()}</tbody>} */}
         </table>
       </div>
     );
   }
 }
-
-export default Users;
+function mapStateToProps(state) {
+  return state.users;
+}
+export default connect(mapStateToProps)(Users);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 
 export default combineReducers({
-  userReducer,
+  users: userReducer,
 });
