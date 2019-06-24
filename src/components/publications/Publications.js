b23317e82ca4fcf285197b2ef1bbb44dcb21a079
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-06-24 16:04:17 -0500

    Share reducer

--- src/components/publications/Publications.js
+++ src/components/publications/Publications.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../../actions/index';
 
 class Publications extends Component {
+  componentDidMount() {
+    if (!this.props.users.length) {
+      this.props.fetchUsers();
+    }
+  }
   render() {
     return <div>{this.props.match.params.id}</div>;
   }
 }
-export default Publications;
+function mapStateToProps(state) {
+  return {
+    users: state.user.users,
+  };
+}
+export default connect(
+  mapStateToProps,
+  { fetchUsers },
+)(Publications);
